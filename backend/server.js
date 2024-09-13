56c8b6f0916251020b40baa86340ef00b924b945
sharmaxsurbhi <sharmaxsurbhi@gmail.com>
2024-09-13 21:20:12 +0530

    adding serverless-http

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const cors = require('cors');
 require('dotenv').config();
+const serverless = require('serverless-http');
+
 
 const app = express();
 
@@ -19,7 +21,17 @@ app.get('/', (req, res) => {
 // Routes
 app.use('/api/users', userRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+
+module.exports.handler = serverless(app);
